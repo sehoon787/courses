3478b7f0e89984eb26aa0d82b213a639d6559c9e
Chanwoo Kim <chanwcom@gmail.com>
2025-06-11 09:02:00 +0900

    Update

--- 2025_spring/xai604/project/run/wav2vec_finetuning.py
+++ 2025_spring/xai604/project/run/wav2vec_finetuning.py
@@ -7,6 +7,7 @@ __author__ = "Chanwoo Kim(chanwcom@gmail.com)"
 # Standard imports
 import glob
 import os
+import io
 
 # Third-party imports
 from transformers import AutoModelForCTC, TrainingArguments, Trainer
@@ -15,81 +16,41 @@ from transformers import AutoProcessor
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Union
 from torch.utils import data
-import tensorflow as tf
 import torch
+import torchaudio
+import webdataset as wds
 import evaluate
 import numpy as np
 
 # Custom imports
-from data import speech_data_helper
 from typing import Any, Dict, List, Optional, Union
 
-# Prevents Tensorflow from using the entire GPU memory.
-#
-# Since we use Tensorflow and Pytorch simultaneously, Tensorflow shouldl not
-# occupy the entire memory. Instead of allocating the entire GPU memory, GPU
-# memory allocated to Tensorflow grows based on its need. Refer to the
-# following website for more information:
-# https://www.tensorflow.org/guide/gpu
-gpus = tf.config.list_physical_devices("GPU")
-if gpus:
-  try:
-    # Currently, memory growth needs to be the same across GPUs.
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-    logical_gpus = tf.config.list_logical_devices("GPU")
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+db_top_dir = "/mnt/data/database"
+train_top_dir = os.path.join(db_top_dir, "stop_music/music_train")
+test_top_dir = os.path.join(db_top_dir, "stop_music/music_test0")
 
-  except RuntimeError as e:
-    # Memory growth must be set before GPUs have been initialized.
-    print(e)
+def preprocess_sample(sample):
+    waveform, sample_rate = torchaudio.load(io.BytesIO(sample['wav']))
+    input_values = processor.feature_extractor(
+        waveform[0], sampling_rate=sample_rate).input_values[0]
 
-db_top_dir = "/home/chanwcom/databases/"
-train_top_dir = os.path.join(db_top_dir, "stop/music_train_tfrecord")
-test_top_dir = os.path.join(db_top_dir,
-                            "stop/test_0_music_random_300_tfrecord")
+    text = sample['txt'].decode('utf-8')
+    labels = processor.tokenizer(text).input_ids
 
-# yapf: disable
-op = speech_data_helper.SpeechDataToWave()
-train_dataset = tf.data.TFRecordDataset(
-    glob.glob(os.path.join(train_top_dir, "*tfrecord-*")),
-              compression_type="GZIP")
-train_dataset = train_dataset.batch(1)
-train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-train_dataset = train_dataset.map(
-    op.process, num_parallel_calls=tf.data.AUTOTUNE)
-# yapf: enable
+    return {"input_values": input_values, "labels": labels}
 
-# yapf: disable
-test_dataset = tf.data.TFRecordDataset(
-    glob.glob(os.path.join(test_top_dir, "*tfrecord-*")),
-              compression_type="GZIP")
-test_dataset = test_dataset.batch(1)
-test_dataset = test_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-test_dataset = test_dataset.map(op.process)
-# yapf: enable
+
+train_dataset = (wds.WebDataset(glob.glob(os.path.join(train_top_dir, "shard-*.tar")))
+            .to_tuple("wav", "txt")
+            .map(lambda sample: { "wav": sample[0], "txt": sample[1] })
+            .map(preprocess_sample))
+test_dataset = (wds.WebDataset(glob.glob(os.path.join(test_top_dir, "shard-*.tar")))
+            .to_tuple("wav", "txt")
+            .map(lambda sample: { "wav": sample[0], "txt": sample[1] })
+            .map(preprocess_sample))
 
 processor = AutoProcessor.from_pretrained("facebook/wav2vec2-base")
 
-class IterDataset(data.IterableDataset):
-    def __init__(self, tf_dataset):
-        self._dataset = tf_dataset
-
-    def __iter__(self):
-        for data in self._dataset:
-            output = {}
-            output["input_values"] = [tf.squeeze(data[0]["SEQ_DATA"]).numpy()]
-            output["input_length"] = tf.squeeze(data[0]["SEQ_LEN"]).numpy()
-            with processor.as_target_processor():
-                output["labels"] = processor(
-                    data[1]["SEQ_DATA"][0].numpy().decode(
-                        "unicode_escape")).input_ids
-
-            yield (output)
-
-
-pytorch_train_dataset = IterDataset(train_dataset)
-pytorch_test_dataset = IterDataset(test_dataset)
 
 def compute_metrics(pred):
     pred_logits = pred.predictions
@@ -117,7 +78,7 @@ class DataCollatorCTCWithPadding:
         # split inputs and labels since they have to be of different lengths and need
         # different padding methods
         input_features = [{
-            "input_values": feature["input_values"][0]
+            "input_values": feature["input_values"]
         } for feature in features]
         label_features = [{
             "input_ids": feature["labels"]
@@ -172,8 +133,8 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=pytorch_train_dataset,
-    eval_dataset=pytorch_test_dataset,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
     tokenizer=processor,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
