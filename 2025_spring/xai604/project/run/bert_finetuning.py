34cc4cf28c0f02c304cd932d47cb4a845ac46694
Chanwoo Kim <chanwcom@gmail.com>
2025-07-03 21:02:49 +0900

    Update

--- 2025_spring/xai604/project/run/bert_finetuning.py
+++ 2025_spring/xai604/project/run/bert_finetuning.py
@@ -7,66 +7,23 @@ __author__ = "Chanwoo Kim(chanwcom@gmail.com)"
 # Standard imports
 import glob
 import os
+from typing import Dict
 
 # Third-party imports
-from transformers import TrainingArguments, Trainer, AutoModelForSequenceClassification, AutoTokenizer
-from datasets import load_dataset
-from torch.utils import data
-import tensorflow as tf
 import torch
 import evaluate
 import numpy as np
+from transformers import TrainingArguments, Trainer, AutoModelForSequenceClassification, AutoTokenizer
+from datasets import load_dataset
+from torch.utils import data
 
 # Custom imports
-from data import speech_data_helper
-
-# Preventing Tensorflow from using the entire GPU memory.
-#
-# Since we use Tensorflow and Pytorch simultaneously, Tensorflow shouldl not
-# occupy the entire memory. Instead of allocating the entire GPU memory, GPU
-# memory allocated to Tensorflow grows based on its need. Refer to the
-# following website for more information:
-# https://www.tensorflow.org/guide/gpu
-gpus = tf.config.list_physical_devices("GPU")
-if gpus:
-  try:
-    # Currently, memory growth needs to be the same across GPUs.
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-    logical_gpus = tf.config.list_logical_devices("GPU")
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-
-  except RuntimeError as e:
-    # Memory growth must be set before GPUs have been initialized.
-    print(e)
-
-db_top_dir = "/home/chanwcom/databases/"
-train_top_dir = os.path.join(db_top_dir, "stop/music_train_tfrecord")
-test_top_dir = os.path.join(db_top_dir,
-                            "stop/test_0_music_random_300_tfrecord")
-
-# yapf: disable
-op = speech_data_helper.SpeechDataToWave()
-train_dataset = tf.data.TFRecordDataset(
-    glob.glob(os.path.join(train_top_dir, "*tfrecord-*")),
-              compression_type="GZIP")
-train_dataset = train_dataset.batch(1)
-train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-train_dataset = train_dataset.map(
-    op.process, num_parallel_calls=tf.data.AUTOTUNE)
-# yapf: enable
-
-# yapf: disable
-test_dataset = tf.data.TFRecordDataset(
-    glob.glob(os.path.join(test_top_dir, "*tfrecord-*")),
-              compression_type="GZIP")
-test_dataset = test_dataset.batch(1)
-test_dataset = test_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-test_dataset = test_dataset.map(op.process)
-# yapf: enable
+import sample_util
 
 model_name = "google-bert/bert-base-uncased"
-
+db_top_dir = "/mnt/data/database"
+train_top_dir = os.path.join(db_top_dir, "stop_music/music_train")
+test_top_dir = os.path.join(db_top_dir, "stop_music/music_test0")
 
 def find_index(inputs):
     intents = [
@@ -84,6 +41,7 @@ def find_index(inputs):
         "START_SHUFFLE_MUSIC",
         "STOP_MUSIC",
         "UNSUPPORTED_MUSIC",
+        "SET_DEFAULT_PROVIDER_MUSIC",
     ]
 
     index = -1
@@ -92,44 +50,35 @@ def find_index(inputs):
             index = i
             break
 
-    assert index >= 0
+    assert index >= 0 assert index >= 0
 
     return index
 
 
-model = AutoModelForSequenceClassification.from_pretrained(model_name,
-                                                           num_labels=14)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-
 def tokenize_function(examples):
-    return tokenizer(examples, padding="max_length", truncation=True)
+    return tokenizer([examples], padding="max_length", truncation=True)
 
+def preprocess_examples(sample: Dict) -> Dict:
+    output = {}
+    output["label"] = find_index(sample["meta"]["intents"][0]["intent"])
+    output["text"] = sample["labels"]
+    tokenized = tokenize_function(output["text"])
+    for key in tokenized.keys():
+        output[key] = tokenized[key][0]
+
+    return output
+
+
+train_dataset = (sample_util.make_dataset(train_top_dir, do_tokenize=False)
+    .map(lambda example: preprocess_examples(example)))
+test_dataset = (sample_util.make_dataset(test_top_dir, do_tokenize=False)
+    .map(lambda example: preprocess_examples(example)))
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name,
+                                                           num_labels=15)
 
 metric = evaluate.load("accuracy")
-
-
-class IterDataset(data.IterableDataset):
-
-    def __init__(self, tf_dataset):
-        self._dataset = tf_dataset
-        op = speech_data_helper.SpeechDataToWave()
-
-    def __iter__(self):
-        for data in self._dataset:
-            output = {}
-            output["label"] = find_index(data[3][0][0])
-            output["text"] = data[1]["SEQ_DATA"][0].numpy().decode("utf-8")
-            tokenized = tokenize_function(output["text"])
-            for key in tokenized.keys():
-                output[key] = tokenized[key]
-
-            yield (output)
-
-
-pytorch_train_dataset = IterDataset(train_dataset)
-pytorch_test_dataset = IterDataset(test_dataset)
-
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
@@ -138,8 +87,9 @@ def compute_metrics(eval_pred):
 
 
 training_args = TrainingArguments(
-    output_dir=
-    "/home/chanwcom/local_repositories/cognitive_workflow_kit/tool/models/bert_model",
+    # Directory to save model checkpoints and outputs.
+    output_dir="/home/chanwcom/local_repositories/cognitive_workflow_kit/tool/"
+               "models/bert_stop_model_final",
     per_device_train_batch_size=40,
     gradient_accumulation_steps=2,
     learning_rate=1e-4,
@@ -160,8 +110,8 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=pytorch_train_dataset,
-    eval_dataset=pytorch_test_dataset,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
     compute_metrics=compute_metrics,
 )
 
